2da490171640593a9d20a21979fa3f627738f55a
SherlockX9 <contact.m3self@gmail.com>
2024-02-22 19:10:03 +0000

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Text2TreeTool
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
